e11d30fd6e4774427be88ca3621249d6855907a1
WeberBsB <138947808+WeberBsB@users.noreply.github.com>
2023-07-09 01:04:37 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+const personagens = document.querySelectorAll('.personagem');
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        if(window.innerWidth < 450){
+         window.scrollTo({top: 0,behavior: 'smooth'}); 
+        }
+        
+        RemoverSelecaodopersonagem();
+
+        personagem.classList.add('selecionado')
+
+        AlterarImagemPersonagemSelecionado(personagem);
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description')
+
+
+    })
+
+})
+
+function AlterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function RemoverSelecaodopersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
